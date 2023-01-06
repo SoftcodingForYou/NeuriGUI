1e71ecc4189d6974c29818df9db0b6d985d4006a
David Baum <github@davidb.anonaddy.com>
2023-01-06 15:40:16 -0300

    Update Board ID

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 #Board unit-specific parameters
-HELMENTID       = '7&74D8485&0&24D7EBA4446A_C00000000' # The unique ID of the board (str)
+HELMENTID       = '7&2F45FB97&0&24D7EBA43B56_C00000000' # The unique ID of the board (str)
 
 #Session-specific parameters
 yrange          = [-500, 500] # List of scalars ([negative, positive]) in order to set figure y axis range

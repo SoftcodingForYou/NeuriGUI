7936063b5f690f7a44054985f48570f70bfb7f7c
David Baum <github@davidb.anonaddy.com>
2023-05-13 00:34:15 -0400

    Cross platform compatibility (keyboard lib) and BT default

--- backend/Helment_configure_board.py
+++ backend/Helment_configure_board.py
@@ -1,6 +1,5 @@
 import serial #Crucial: Install using pip3 install "pyserial", NOT "serial"
 import serial.tools.list_ports
-import keyboard
 import parameters                   as p
 from encodings                      import utf_8
 
@@ -39,7 +38,7 @@ class ConfigureBoard:
                 # Bluetooth device query
                 self.av_ports["BT"] = port.device
                 print('Found Helment connected via Bluetooth')
-            elif 'Silicon Labs CP210x USB to UART Bridge' in port.description:
+            elif 'UART Bridge' in port.description:
                 # USB device query
                 self.av_ports["USB"] = port.device
                 print('Found Helment connected via USB')
@@ -67,65 +66,44 @@ class ConfigureBoard:
         self.ser            = serial.Serial()
         self.ser.baudrate   = p.baud_rate
         self.ser.timeout    = p.time_out
-        if self.av_ports["BT"] != None:
+        if self.av_ports["BT"] != None: # Priority
             self.ser.port   = self.av_ports["BT"]
+            self.des_state  = 3
             str_comtype = 'Board prepared to receive input via Bluetooth'
-        elif self.av_ports["USB"] != None:
+        elif self.av_ports["USB"] != None: # Fallback solution
             self.ser.port   = self.av_ports["USB"]
+            self.des_state  = 2
             str_comtype = 'Board prepared to receive input via USB'
         self.ser.open() # Resets automatically the board
         print(str_comtype)
-        
 
-    def query_input(self):
-        # =================================================================
-        # Listen for valid key presses and send state to board
-        # Based on input, call for different methods of this class (ie 
-        # reconfigure)
-        # INPUT
-        #   states          = Valid keyboard presses (list of strings of 
-        #                     numbers)
-        # OUTPUT
-        #   key             = number of keyboard press or default (integer)
-        # =================================================================
-
-        key                 = int(-1) # Non-triggering value
-
-        if any(keyboard.is_pressed(key) for key in self.states):
-            key = keyboard.read_key()
-            key = int(key)
-
-            if key == 0: # "Non-sense" mode (sends defined string)
-                print('Asking if board alive')
-            elif key == 1: # Standby mode of the board
-                print('Putting board to standby. Waiting for new state...')
-            elif key == 2: # Start sampling
-                if self.av_ports["USB"] == None:
-                    raise Exception('USB was chosen but is not available')
-                print('Preparing sampling via USB')
-            elif key == 3: # Start sampling
-                if self.av_ports["BT"] == None:
-                    raise Exception('Bluetooth was chosen but is not available')
-                print('Preparing sampling via Bluetooth')
-            elif key == 4: # Configuration mode
-                print('Not implemented: Send configuration')
-            elif key == 5: # Reset device
-                print('Resetting device')
-
-            self.inform_board(key)
-
-        return key
+        self.inform_board(self.des_state)
 
 
     def inform_board(self, state):
 
-        state = str(state)
+        if state == 0: # "Non-sense" mode (sends defined string)
+            print('Asking if board alive')
+        elif state == 1: # Standby mode of the board
+            print('Putting board to standby. Waiting for new state...')
+        elif state == 2: # Start sampling
+            if self.av_ports["USB"] == None:
+                raise Exception('USB was chosen but is not available')
+            print('Preparing sampling via USB')
+        elif state == 3: # Start sampling
+            if self.av_ports["BT"] == None:
+                raise Exception('Bluetooth was chosen but is not available')
+            print('Preparing sampling via Bluetooth')
+        elif state == 4: # Configuration mode
+            print('Not implemented: Send configuration')
+        elif state == 5: # Reset device
+            print('Resetting device')
 
         # Make board aware of key press: Set state of board
-        state = bytes(state, 'utf-8')
+        state = bytes(str(state), 'utf-8')
         self.ser.write(state)
         self.ser.close()
-        print('Communicated changes to board: ' + str(state))
+        print('Communicated changes to board')
 
 
     def reconfigure(self, cong_string):
